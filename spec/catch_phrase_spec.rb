054607cbacf495f7535fb5019d31aa71222ebf3f
Yevgeniy Piliposyans <piliposyans@icloud.com>
2020-08-06 18:36:05 +0000

    Automatically backed up by Learn

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -1,10 +1,4 @@
-ruby 
-describe "#catch_phrase" do
-  it "puts out a catch phrase" do
-  phrase = "It's-a me, Mario!"
-    expect{catch_phrase(phrase)}.to output("It's-a me, Mario!\n").to_stdout
-  end
-end
+
 
 
 
